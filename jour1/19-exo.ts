870046510ce877243d0d110bbc55b39d20f3d818
Malik H <malik.h@webdevpro.net>
2022-01-06 16:55:15 +0100

    généricité sur les class

---
+++ jour1/19-exo.ts
@@ -0,0 +1,26 @@
+export class Produit<Stock, Prix> {
+    s : Stock;
+    p : Prix ;
+    constructor( s : Stock , p : Prix ){
+        this.s = s;
+        this.p = p  ;
+    }
+    getS() : Stock{
+        return this.s; 
+    }
+    getP() : Prix{
+        return this.p;
+    }
+}
+
+const p1 = new Produit({id : 1 , qte : 10 , nom : "Produit 1"} , {id : 1 , prix : 20})
+p1.getS().nom
+
+interface interfacePrix {
+    id : number , prix : number
+}
+
+const p2 = new Produit<number , interfacePrix>(20 , {id : 1, prix : 3}); 
+
+p2.getS()
+p2.getP().prix ; 
