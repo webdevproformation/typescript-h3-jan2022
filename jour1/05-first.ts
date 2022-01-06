1f5a2a5d58295c11f782b4b4e4e7d349cfcf49bc
Malik H <malik.h@webdevpro.net>
2022-01-06 11:29:46 +0100

    exo 5

--- jour1/05-first.ts
+++ jour1/05-first.ts
@@ -40,7 +40,27 @@ let l : unknown ; // variable qui existe dont on définira par la suite le type
 // types avancés Array et Object function ....
 
 
+/**
+Créer le fichier 06-exo.ts 
+ce fichier contient les instructions suivantes :
+// est ce que une de ces instructions va déclencher une erreur de typage ??? si oui lesquels et POURQUOI ??
+let a:number ;
+a = 2;
 
+let b:any;
+b = 20;
+b = false;
+b = "du texte";
+
+let c ;
+c = 2;
+c = "autre texte"
+
+
+let d = false ;
+d = true ;
+d = "texte final"
+ */
 
 
 
