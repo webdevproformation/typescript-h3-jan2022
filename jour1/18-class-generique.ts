4e6ff0e0fc8e2369094193e9f84bb019accc6443
Malik H <malik.h@webdevpro.net>
2022-01-06 16:41:19 +0100

    exo 19

--- jour1/18-class-generique.ts
+++ jour1/18-class-generique.ts
@@ -12,5 +12,14 @@ class Forme<T> {
 }
 
 const f = new Forme({id: 1 , largeur : 20})
-f.getProp().id ; 
+f.getProp().id ;
+
+// créer un nouveau fichier 19-exo.ts 
+
+// class Produit et qui contient 2 propriétés générique dans son constructeur Stock / Prix 
+
+// créer cette class et créer les getter pour récupérer les valeurs de ces deux propriété de la class 
+
+
+
 
