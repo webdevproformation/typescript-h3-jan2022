d1f8dcf516a4cf1610f133aee65d9b28f1a79f6e
Malik H <malik.h@webdevpro.net>
2022-01-06 15:54:25 +0100

    correction exo 15

---
+++ jour1/15-exo.ts
@@ -0,0 +1,16 @@
+export {}
+type Etudiant = {id : number , nom : string }
+function get(nom : string) : string | Etudiant {
+    if(nom.length === 0 ){
+        return nom;
+    }
+    return {
+        id : 1 ,
+        nom : nom
+    }
+}
+console.log((get("") as string).toUpperCase());
+console.log((<string>get("") ).toUpperCase());
+
+(get("Alain") as Etudiant).nom.toUpperCase();
+(<Etudiant>get("Alain") ).nom.toUpperCase();
