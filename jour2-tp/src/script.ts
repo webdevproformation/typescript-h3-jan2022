107ffc762c6ae1526c8016d8bddf007b8d5c8018
Malik H <malik.h@webdevpro.net>
2022-01-07 16:21:24 +0100

    enoncé

--- jour2-tp/src/script.ts
+++ jour2-tp/src/script.ts
@@ -1,6 +1,5 @@
 import {addLog , verif , interfaceData} from "./lib.js" 
 // pour pouvoir exécuter ton ts => js dans navigateur => .js
-
 const forms = document.querySelectorAll("form") 
 forms.forEach( form => {
     form.addEventListener( "submit" , function(e : Event ){
@@ -24,3 +23,11 @@ forms.forEach( form => {
 // vérifier que les deux champs ne sont pas vides et age > 0 et < 100
 // verifier que nom contient au moins 5 caractères et au plus 250
 
+// cas pratique : créer un nouveau dossier 
+
+// jour2-tp2
+// index.html 
+// form > input de type text / checkbox bouton submit 
+// cliquer pour soumettre ajouter en dessous du formulaire 
+// libellé / status (coché => realisé / non coché => en attente )
+// réaliser toute la logique via typescript (utiliser le fichier tsconfig et le système de module es6 pour organiser votre code )
