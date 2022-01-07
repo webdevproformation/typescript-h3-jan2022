1c99fb983974cd4fac52120a7b6d76f701513e3c
Malik H <malik.h@webdevpro.net>
2022-01-07 14:37:41 +0100

    enoncé

--- jour2-tp/src/script.ts
+++ jour2-tp/src/script.ts
@@ -17,3 +17,8 @@ function addLog( data : interfaceData ){
     const div = document.querySelector(".log") as HTMLDivElement ;
     div.innerHTML += `<p>${data.nom} - ${data.age}</p>`;
 }
+// cas pratique :
+// ajouter les vérifications nécessaires avant d'ajouter les informations dans la page web
+// vérifier que les deux champs ne sont pas vides et age > 0 et < 100
+// verifier que nom contient au moins 5 caractères et au plus 250
+
