d54ce0b9db7de0bcd22d36a063bf048ddd207819
Malik H <malik.h@webdevpro.net>
2022-01-07 13:48:54 +0100

    start du projet

--- jour2/07-exo.js
+++ jour2/07-exo.js
@@ -1,4 +1,19 @@
-"use strict";
-// npm init --yes 
-// npm install ora
-exports.__esModule = true;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports", "ora"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    exports.__esModule = true;
+    var ora_1 = __importDefault(require("ora"));
+    console.log(ora_1["default"]);
+    console.log("tata");
+});
+//# sourceMappingURL=07-exo.js.map
--- jour2/07-exo.ts
+++ jour2/07-exo.ts
@@ -1,5 +1,14 @@
 // npm init --yes 
 // npm install ora
 
-// import ora from "./node_modules/ora/index.js"
+import ora from "ora"
+ console.log(ora);  
+console.log("tata"); 
 
+// tsconfig.json =>  "moduleResolution": "node", 
+//  "files": ["07-exo.ts" ]  
+// package.json => "type": "module",
+
+// premier terminal => tsc 
+// 2ème terminal => nodemon 07-exo.js
+// npm i -g nodemon
